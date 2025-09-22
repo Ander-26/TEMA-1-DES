e0b9fe669c28b0869f70990438ebe58125935486
Ander-26 <silvacastellanoandersson@gmail.com>
2025-09-22 19:34:42 +0100

    Add files via upload

---
+++ matrizde5.php
@@ -0,0 +1,13 @@
+<?php
+$i = 0;
+$j = 0;
+
+for ($i = 0; $i < 5; $i++){
+    for ($j =0; $j< 5; $j ++){
+        if ($i == $j){
+            echo "  x";
+        }else{ 
+            echo "  o"; 
+        }}
+         echo "\n";
+}
